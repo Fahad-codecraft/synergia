c1013b653d1a3a1f5eb7bab8dbf564b741ad0cc7
Fahad Devnikar <130536727+Fahad-codecraft@users.noreply.github.com>
2025-03-04 12:59:31 +0530

    Update EventPopup.tsx

--- src/components/EventPopup.tsx
+++ src/components/EventPopup.tsx
@@ -116,24 +116,24 @@ const EventPopup: React.FC<EventPopupProps> = ({ event, onClose }) => {
                                 <img src="/icons/calendar.svg" className='w-5 h-5' />
                                 <span>{event.date}</span>
                             </div>
-                            {event.time && (
+{/*                             {event.time && (
                                 <div className="flex items-center gap-2 text-gray-300">
                                     <img src="/icons/clock.svg" className='w-5 h-5' />
                                     <span>{event.time}</span>
                                 </div>
-                            )}
+                            )} */}
                             {event.location && (
                                 <div className="flex items-center gap-2 text-gray-300">
                                     <img src="/icons/mappin.svg" className='w-5 h-5' />
                                     <span>{event.location}</span>
                                 </div>
                             )}
-                            {event.capacity && (
+{/*                             {event.capacity && (
                                 <div className="flex items-center gap-2 text-gray-300">
                                     <img src="/icons/users.svg" className='w-5 h-5' />
                                     <span>{event.capacity}</span>
                                 </div>
-                            )}
+                            )} */}
                         </div>
 
                         <p className="text-gray-300 leading-relaxed mb-8">
